6284e762e080861ef8daad66ef16b82adf054e51
Jiadong Wu <wuplus1992@gmail.com>
2015-04-07 02:18:18 -0400

    update js

---
+++ JS/login.js
@@ -0,0 +1,39 @@
+$(function() {
+    $baseURL = "http://localhost:7654/User/login/";
+
+    $email = $(".email_input_wrapper > input");
+    $password = $(".password_input_wrapper > input");
+
+    $(".login-button").on("click", function() {
+        if ($email.val() == "") alert("1");
+        else if ($password.val() == "") alert("2");
+        else {
+            var person = {
+                email: $email.val(),
+                password: $password.val()
+            }
+            console.log(JSON.stringify(person));
+            $.ajax({
+                type: 'POST',
+                url: $baseURL,
+                dataType: "json",
+                contentType: "application/json",
+                data: JSON.stringify(person),
+                statusCode: {
+                    200: function(response) {
+                        alert("success" + response);
+                        if (response == "1") {
+                            alert("wrong username or password");
+                        } else {
+                            window.location.href = response;
+                        }
+
+                    },
+                    500: function() { //Nothing to save to Employee_Company
+                        alert("Login error");
+                    }
+                }
+            });
+        }
+    });
+});
---
+++ JS/register.js
@@ -0,0 +1,34 @@
+$(function() {
+    $baseURL = "http://localhost:7654/User/";
+
+    $email = $("#email");
+    $username = $("#username");
+    $password = $("#password");
+
+    $(".register-button").on("click", function() {
+        if ($email.val() == "") alert("1");
+        else if ($username.val() == "") alert("2");
+        else if ($password.val() == "") alert("3");
+        else {
+            var person = {
+                username: $username.val(),
+                password: $password.val(),
+                email: $email.val()
+            }
+            console.log(JSON.stringify(person));
+            $.ajax({
+                type: 'PUT',
+                url: $baseURL,
+                dataType: "json",
+                contentType: "application/json",
+                data: JSON.stringify(person),
+                success: function(person) {
+                    alert("Register success");
+                },
+                error: function() {
+                    alert("Register error");
+                }
+            });
+        }
+    });
+});
